2c2089e6ffb68b320cb5b815310e106948748600
Alexander Riedel <54716527+alexriedel1@users.noreply.github.com>
2023-08-10 19:48:52 +0200

    params

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -58,7 +58,7 @@ class AlignedDataset(BaseDataset):
         #    #B = A.resize((256, 256))
 
         # apply the same transform to both A and B
-        transform_params = get_params(self.opt, A.size, A)
+        transform_params = get_params(self.opt, A.size)
         transform = get_transform(self.opt, transform_params, grayscale=(self.input_nc == 1))
         #B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1))
 
